a5eb2e56dcd2ce45d3eb8191ec52bc5ccdb2c1af
Basil Akavity <vakavity@gmail.com>
2024-11-15 22:16:24 +0300

    added NewTest

--- src/test/java/org/akavity/tests/NewTest.java
+++ src/test/java/org/akavity/tests/NewTest.java
@@ -1,16 +1,23 @@
 package org.akavity.tests;
 
+import org.akavity.steps.CatalogSteps;
 import org.akavity.steps.HeaderSteps;
-import org.akavity.utils.Utils;
+import org.akavity.steps.NavigationSteps;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class NewTest extends BaseTest {
     HeaderSteps headerSteps = new HeaderSteps();
-    Utils utils = new Utils();
+    NavigationSteps navigationSteps = new NavigationSteps();
+    CatalogSteps catalogSteps = new CatalogSteps();
 
     @Test
     public void catalogNavigation() {
         headerSteps.clickCatalogButton();
-        utils.sleep(3000);
+        navigationSteps.clickMainListItem("Женщинам");
+        navigationSteps.clickDropListItem("Одежда для дома", "Брюки и шорты");
+
+        String actual = catalogSteps.extractTextFromTitle();
+        Assert.assertEquals(actual, "Брюки и шорты");
     }
 }
