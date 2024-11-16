a070d143607dfc47ea498eab63b34de36bd48ad9
Basil Akavity <vakavity@gmail.com>
2024-11-16 19:56:59 +0300

    added locators

--- src/main/java/org/akavity/pages/CatalogPage.java
+++ src/main/java/org/akavity/pages/CatalogPage.java
@@ -8,6 +8,18 @@ import static com.codeborne.selenide.Selenide.$;
 public class CatalogPage {
     private final SelenideElement tileField = $(By.cssSelector("h1[class='catalog-title']"));
 
+    public SelenideElement getFilterButton(String name) {
+        return $(By.xpath("//button[contains(@class,'dropdown-filter') and contains(text(),'" + name + "')]"));
+    }
+
+    public SelenideElement getRadioButton(String name) {
+        return $(By.xpath("//span[@class='radio-with-text__text' and .='" + name + "']"));
+    }
+
+    public SelenideElement getCheckBox(String name) {
+        return $(By.xpath("//span[@class='checkbox-with-text__text' and .='" + name + "']"));
+    }
+
     public SelenideElement getTileField() {
         return tileField;
     }
