682f93d26a21b701536aa7edcf930b838239e874
Basil Akavity <vakavity@gmail.com>
2024-11-17 21:47:26 +0300

    added locators

--- src/main/java/org/akavity/pages/CatalogPage.java
+++ src/main/java/org/akavity/pages/CatalogPage.java
@@ -8,16 +8,33 @@ import static com.codeborne.selenide.Selenide.$;
 public class CatalogPage {
     private final SelenideElement tileField = $(By.cssSelector("h1[class='catalog-title']"));
 
-    public SelenideElement getFilterButton(String name) {
-        return $(By.xpath("//button[contains(@class,'dropdown-filter') and contains(text(),'" + name + "')]"));
+    // Drop Down Filter
+    private final SelenideElement minPriceDDF = $(By.cssSelector("div[class*='filter__price'] input[name*='start']"));
+    private final SelenideElement maxPriceDDF = $(By.cssSelector("div[class*='filter__price'] input[name*='end']"));
+    private final SelenideElement priceReadyButtonDDF = $(By.xpath("//button[contains(@class,'filter-btn') and contains(.,'Готово')]"));
+
+    public SelenideElement getButtonDDF(String name) {
+        return $(By.xpath("//div[contains(@class,'filter__btn') and contains(.,'" + name + "')]"));
     }
 
-    public SelenideElement getRadioButton(String name) {
-        return $(By.xpath("//span[@class='radio-with-text__text' and .='" + name + "']"));
+    public SelenideElement getRadioButtonDDF(String name) {
+        return $(By.xpath("//span[contains(@class,'radio-with-text') and contains(.,'" + name + "')]"));
     }
 
-    public SelenideElement getCheckBox(String name) {
+    public SelenideElement getCheckBoxDDF(String name) {
         return $(By.xpath("//span[@class='checkbox-with-text__text' and .='" + name + "']"));
+    }
+
+    public SelenideElement getMinPriceDDF() {
+        return minPriceDDF;
+    }
+
+    public SelenideElement getMaxPriceDDF() {
+        return maxPriceDDF;
+    }
+
+    public SelenideElement getPriceReadyButtonDDF() {
+        return priceReadyButtonDDF;
     }
 
     public SelenideElement getTileField() {
