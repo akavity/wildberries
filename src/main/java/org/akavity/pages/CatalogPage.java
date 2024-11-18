f1b80f2caacbaf88e2557b351c990cffd2ff6b1b
Basil Akavity <vakavity@gmail.com>
2024-11-18 10:45:29 +0300

    added step to CatalogSteps

--- src/main/java/org/akavity/pages/CatalogPage.java
+++ src/main/java/org/akavity/pages/CatalogPage.java
@@ -1,14 +1,21 @@
 package org.akavity.pages;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class CatalogPage {
     private final SelenideElement tileField = $(By.cssSelector("h1[class='catalog-title']"));
+    private final ElementsCollection pricesFields = $$(By.cssSelector("span[class='price__wrap'] ins"));
 
     public SelenideElement getTileField() {
         return tileField;
+    }
+
+    public ElementsCollection getPricesFields() {
+        return pricesFields;
     }
 }
--- src/main/java/org/akavity/steps/CatalogSteps.java
+++ src/main/java/org/akavity/steps/CatalogSteps.java
@@ -1,19 +1,33 @@
 package org.akavity.steps;
 
+import com.codeborne.selenide.ElementsCollection;
 import io.qameta.allure.Step;
 import lombok.extern.log4j.Log4j2;
 import org.akavity.pages.CatalogPage;
+import org.akavity.utils.Utils;
+
+import java.util.function.Predicate;
 
 import static com.codeborne.selenide.Condition.visible;
 
 @Log4j2
 public class CatalogSteps {
     CatalogPage catalogPage = new CatalogPage();
+    Utils utils = new Utils();
 
     @Step
     public String extractTextFromTitle() {
         String title = catalogPage.getTileField().shouldBe(visible).text();
         log.info("Catalog title: {}", title);
         return title;
+    }
+
+    @Step
+    public boolean areProductPricesWithinLimit(String min, String max) {
+        log.info("Check product prices \n min price: {} \n max price: {}", min, max);
+        utils.sleep(1500);
+        ElementsCollection prices = catalogPage.getPricesFields();
+        Predicate<Double> predicate = p -> (p >= Integer.parseInt(min) && p <= Integer.parseInt(max));
+        return utils.relationalMethod(prices, predicate);
     }
 }
