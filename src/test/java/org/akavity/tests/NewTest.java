2532a6548c2f436cae05f99519b53dad8bf152d3
Basil Akavity <vakavity@gmail.com>
2024-11-18 12:22:39 +0300

    added sortProductsByPrice test

--- src/test/java/org/akavity/tests/NewTest.java
+++ src/test/java/org/akavity/tests/NewTest.java
@@ -2,7 +2,9 @@ package org.akavity.tests;
 
 import org.akavity.annotations.TestData;
 import org.akavity.models.CatalogData;
+import org.akavity.models.PriceData;
 import org.akavity.steps.CatalogSteps;
+import org.akavity.steps.FilterDropDownSteps;
 import org.akavity.steps.HeaderSteps;
 import org.akavity.steps.NavigationSteps;
 import org.akavity.utils.JsonReader;
@@ -13,6 +15,7 @@ public class NewTest extends BaseTest {
     HeaderSteps headerSteps = new HeaderSteps();
     NavigationSteps navigationSteps = new NavigationSteps();
     CatalogSteps catalogSteps = new CatalogSteps();
+    FilterDropDownSteps filterDDSteps = new FilterDropDownSteps();
 
     @TestData(jsonFile = "catalogData", model = "CatalogData")
     @Test(description = "Catalog navigation", dataProviderClass = JsonReader.class, dataProvider = "getData")
@@ -23,5 +26,17 @@ public class NewTest extends BaseTest {
 
         String actual = catalogSteps.extractTextFromTitle();
         Assert.assertEquals(actual, catalog.getTitle());
+    }
+
+    @TestData(jsonFile = "priceData", model = "PriceData")
+    @Test(description = "Check that product prices are within the limit", dataProviderClass = JsonReader.class, dataProvider = "getData")
+    public void sortProductsByPrice(PriceData price) {
+        headerSteps.clickCatalogButton();
+        navigationSteps.clickMainListItem(price.getMainListItem());
+        navigationSteps.clickDropListItem(price.getFirstDropListItem(), price.getSecondDropListItem());
+        filterDDSteps.clickButtonDDF(price.getButton());
+        filterDDSteps.enterMinMaxAmount(price.getMinPrice(), price.getMaxPrice());
+
+        Assert.assertTrue(catalogSteps.areProductPricesWithinLimit(price.getMinPrice(), price.getMaxPrice()));
     }
 }
