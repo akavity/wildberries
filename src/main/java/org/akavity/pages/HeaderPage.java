ca4a2558af67814065e5772827017e701f4b8aab
Basil Akavity <vakavity@gmail.com>
2024-12-10 18:04:21 +0300

    added locators

--- src/main/java/org/akavity/pages/HeaderPage.java
+++ src/main/java/org/akavity/pages/HeaderPage.java
@@ -9,6 +9,16 @@ public class HeaderPage {
     private final SelenideElement catalogButton = $(By.cssSelector("button[data-wba-header-name='Catalog']"));
     private final SelenideElement searchField = $(By.cssSelector("input[id='searchInput']"));
     private final SelenideElement loupeButton = $(By.cssSelector("button[id='applySearchBtn']"));
+    private final SelenideElement addressButton = $(By.cssSelector("div[class*='item-addresses']"));
+    private final SelenideElement iframe = $(By.cssSelector("iframe[class='service-page__iframe']"));
+
+    public SelenideElement getServiceMenuItem(String item) {
+        return $(By.xpath("//li[contains(@class,'service-menu__item')]/a[contains(text(),'" + item + "')]"));
+    }
+
+    public SelenideElement getServiceMenuTitle(String title) {
+        return $(By.xpath("//div[@class='service-page-iframe']//h1[contains(text(),'" + title + "')]"));
+    }
 
     public SelenideElement getCatalogButton() {
         return catalogButton;
@@ -20,5 +30,13 @@ public class HeaderPage {
 
     public SelenideElement getLoupeButton() {
         return loupeButton;
+    }
+
+    public SelenideElement getAddressButton() {
+        return addressButton;
+    }
+
+    public SelenideElement getIframe() {
+        return iframe;
     }
 }
