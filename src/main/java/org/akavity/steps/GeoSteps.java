43cb026966efb17182e3784731316c3b0b6dfaa6
Basil Akavity <vakavity@gmail.com>
2024-12-13 19:29:22 +0300

    save

--- src/main/java/org/akavity/steps/GeoSteps.java
+++ src/main/java/org/akavity/steps/GeoSteps.java
@@ -1,20 +1,20 @@
 package org.akavity.steps;
 
+import io.qameta.allure.Step;
 import lombok.extern.log4j.Log4j2;
 import org.akavity.pages.GeoPage;
-import org.testng.annotations.Test;
 
 @Log4j2
 public class GeoSteps {
     GeoPage geoPage = new GeoPage();
 
-    @Test
+    @Step
     public void clickAddressButton(String address) {
         log.info("Click address button: {}", address);
-        geoPage.getAddressButton(address).click();
+        geoPage.getAddressButton(address).scrollTo().click();
     }
 
-    @Test
+    @Step
     public void clickTakeFromHereButton() {
         log.info("Click button 'Take from here'");
         geoPage.getTakeFromHereButton().click();
