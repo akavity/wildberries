7c07212c0fcaf44b80dd7202a096c4e3553fdf15
Basil Akavity <vakavity@gmail.com>
2025-03-20 18:34:46 +0300

    locators have been added

--- src/main/java/org/akavity/pages/InfoPage.java
+++ src/main/java/org/akavity/pages/InfoPage.java
@@ -10,13 +10,21 @@ public class InfoPage {
         return $(By.xpath("//div[@class='service-menu']//a[contains(text(),'" + item + "')]/.."));
     }
 
+    public SelenideElement getDropDownTitle(String title) {
+        return $(By.xpath("//h2[contains(@class,'dropdown-t') and contains(text(),'" + title + "')]"));
+    }
+
+    public SelenideElement getDropDownContent(String title, String cont) {
+        return $(By.xpath("//h2[contains(@class,'dropdown-titl') and contains(text(),'" + title + "')]/..//p[contains(text(),'" + cont + "')]"));
+    }
+
     // Frequently asked questions (FAQ)
     public SelenideElement getFAQMenuItem(String item) {
         return $(By.xpath("//div[contains(@class,'faq-menu')]//span[contains(text(),'" + item + "')]/.."));
     }
 
     public SelenideElement getFAQTitle(String title) {
-        return $(By.xpath("//div[contains(@class,'faq-contain')]//h1[contains(text(),'" + title + "')]"));
+        return $(By.xpath("//div[contains(@class,'faq-contain')]//h1[contains(text(),'\" + title + \"')]"));
     }
 
     public SelenideElement getFAQ(String q) {
