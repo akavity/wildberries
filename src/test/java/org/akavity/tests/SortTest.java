459baf387893c4f774bd365ebbd09c96bdad09c9
Basil Akavity <vakavity@gmail.com>
2025-04-02 11:24:14 +0300

    sortByAscendingPrise test has been added

--- src/test/java/org/akavity/tests/SortTest.java
+++ src/test/java/org/akavity/tests/SortTest.java
@@ -1,9 +1,10 @@
 package org.akavity.tests;
 
 import org.akavity.annotations.TestData;
+import org.akavity.models.sortTest.AscendingPriceData;
 import org.akavity.models.sortTest.PriceData;
 import org.akavity.steps.CatalogSteps;
-import org.akavity.steps.FilterDropDownSteps;
+import org.akavity.steps.FiltersBlockSteps;
 import org.akavity.steps.HeaderSteps;
 import org.akavity.steps.NavigationSteps;
 import org.akavity.utils.JsonReader;
@@ -13,18 +14,30 @@ import org.testng.annotations.Test;
 public class SortTest extends BaseTest {
     HeaderSteps headerSteps = new HeaderSteps();
     NavigationSteps navigationSteps = new NavigationSteps();
-    FilterDropDownSteps filterDDSteps = new FilterDropDownSteps();
+    FiltersBlockSteps filtersBlockSteps = new FiltersBlockSteps();
     CatalogSteps catalogSteps = new CatalogSteps();
 
     @TestData(jsonFile = "priceData", model = "PriceData", folder = "sortTest")
-    @Test(description = "Check that product prices are within the limit", dataProviderClass = JsonReader.class, dataProvider = "getData")
+    @Test(description = "Sort products by price within set limits", dataProviderClass = JsonReader.class, dataProvider = "getData")
     public void sortProductsByPrice(PriceData price) {
         headerSteps.clickCatalogButton();
         navigationSteps.clickMainListItem(price.getMainListItem());
         navigationSteps.clickDropListItem(price.getMainListItem(), price.getFirstDropListItem(), price.getSecondDropListItem());
-        filterDDSteps.clickButtonDDF(price.getButton());
-        filterDDSteps.enterMinMaxAmount(price.getMinPrice(), price.getMaxPrice());
+        filtersBlockSteps.clickFilterButton(price.getButton());
+        filtersBlockSteps.enterMinMaxAmount(price.getMinPrice(), price.getMaxPrice());
 
         Assert.assertTrue(catalogSteps.areProductPricesWithinLimit(price.getMinPrice(), price.getMaxPrice()));
+    }
+
+    @TestData(jsonFile = "ascendingPriceData", model = "AscendingPriceData", folder = "sortTest")
+    @Test(description = "Sort products by ascending price", dataProviderClass = JsonReader.class, dataProvider = "getData")
+    public void sortByAscendingPrice(AscendingPriceData ascendingPrice) {
+        headerSteps.clickCatalogButton();
+        navigationSteps.clickMainListItem(ascendingPrice.getMainListItem());
+        navigationSteps.clickDropListItem(ascendingPrice.getMainListItem(), ascendingPrice.getFirstDropListItem(), ascendingPrice.getSecondDropListItem());
+        filtersBlockSteps.clickSorterButton();
+        filtersBlockSteps.selectSortType(ascendingPrice.getSortType());
+
+        Assert.assertTrue(catalogSteps.checkSortByAscendingPrice(ascendingPrice.getElements()));
     }
 }
